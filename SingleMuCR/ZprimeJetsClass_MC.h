11909e97531bc2ce7fee602e1902e6ecba13f4d5
ekoenig4 <ekoenig4@wisc.edu>
2018-02-23 13:21:24 -0600

    fixed merge

--- SingleMuCR/ZprimeJetsClass_MC.h
+++ SingleMuCR/ZprimeJetsClass_MC.h
@@ -58,18 +58,6 @@ public :
    double PF12PtFrac_ID_1,PF12PtFrac_ID_2,dR_PF12_ID_1,dR_PF12_ID_2,PF123PtFrac_ID_2;
    //CR variables
    int lepindex;
-<<<<<<< HEAD
-   double lepton_mass,lepton_pt,Recoil;
-   float leptoMET_phi_to_use;
-   //getPFCandidates
-   int TotalPFCandidates, ChargedPFCandidates,NeutralPFCandidates,GammaPFCandidates;
-   
-   TH1F *h_nVtx[16],*h_metcut, *h_dphimin,*h_metFilters[16],*h_pfMETall[16],*h_pfMET200[16],*h_nJets[16],*h_pfMET[16],*h_pfMETPhi[16],*h_j1nCategory1[16],*h_j1nCategory2[16],*h_j1dRPF12_ID_1[16],*h_j1dRPF12_ID_2[16];
-   TH1F *h_j1Pt[16], *h_j1Eta[16], *h_j1Phi[16], *h_j1etaWidth[16], *h_j1phiWidth[16],*h_j1nCons[16], *h_j1PF12PtFrac_ID_1[16], *h_j1PF12PtFrac_ID_2[16],*h_j1PFPtFrac_ID_2[16];  
-   TH1F *h_j1TotPFCands[16], *h_j1ChPFCands[16], *h_j1NeutPFCands[16], *h_j1GammaPFCands[16], *h_j1CHF[16], *h_j1NHF[16], *h_j1ChMultiplicity[16], *h_j1NeutMultiplicity[16],*h_j1Mt[16]; 
-   //CR histograms 
-   TH1F *h_LeptonPt[16], *h_LeptonEta[16],*h_LeptonPhi[16],*h_leptonPt[16],*h_leptonM[16];
-=======
    double lepton_pt,Recoil;
    //float leptoMET_phi_to_use;
    //getPFCandidates
@@ -80,7 +68,6 @@ public :
    TH1F *h_j1TotPFCands[16], *h_j1ChPFCands[16], *h_j1NeutPFCands[16], *h_j1GammaPFCands[16], *h_j1CHF[16], *h_j1NHF[16], *h_j1ChMultiplicity[16], *h_j1NeutMultiplicity[16],*h_j1Mt[16]; 
    //CR histograms 
    TH1F *h_LeptonPt[16], *h_LeptonEta[16],*h_LeptonPhi[16];
->>>>>>> 97d6dc6ebfa65ee0fe2c4d0fe2e98a261137cf6c
    TH1F *h_recoil[16];
    TH1D *h_cutflow;
 
