7fe6f988ab960e222a6842ca173d9a751467f5e5
Usama <uhussain@wisc.edu>
2018-07-18 08:51:55 -0500

    Separate submission for signal samples because of weird naming

--- SystUncertainties/JetMetEnergyScale/submitSignal.sh
+++ SystUncertainties/JetMetEnergyScale/submitSignal.sh
@@ -2,7 +2,7 @@ DATE=July10
 
 echo "Do the Signal samples"
 
-./rootcom ZprimeJetsClass_MC analyze
+./rootcom ZprimeJetsClass_MC_signal analyze
 
 Mx="10 50 100"
 Mv="100 200 500 1000 1500 1800 2000 2500 3500"
@@ -12,7 +12,7 @@ for dm in $Mx;do
     dir="Mx${dm}_Mv${Med}_MINIAOD"
     echo " "
     echo "----------------------"   ${dir}  "-------------------------"
-    ./MakeCondorFiles.csh analyze /hdfs/store/user/uhussain/${dir}-ZprimeSignalJobs_${DATE}/ postSignal_Mx${dm}_Mv${Med}.root -1 1000 signal_Mx${dm}_Mv${Med} PU_Central.root
+    ./SubmitCondor_signal analyze /hdfs/store/user/uhussain/${dir}-ZprimeSignalJobs_${DATE}/ postSignal_Mx${dm}_Mv${Med}.root -1 1000 signal_Mx${dm}_Mv${Med} split_1
   done
 done
 
