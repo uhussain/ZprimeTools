b7a6c3ba2c3a2b32ad6d33e5d4553c8bfa4d9ed5
Usama <uhussain@wisc.edu>
2019-02-26 07:13:12 -0600

    Latest PU situation

--- PileupCorrectionRecipe/getDataDistribution.sh
+++ PileupCorrectionRecipe/getDataDistribution.sh
@@ -1,7 +1,10 @@
 # Following https://hypernews.cern.ch/HyperNews/CMS/get/luminosity/613/2/1/1/1.html
-json=processedLumisG.json
-pileup=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/PileUp/pileup_latest.txt
-bins=75
+#json=ZZ4l2018Data/ZZ2018Processed_Jan28.json
+json=ZZ4l2017Data/13Feb2019/ProcessedLumis.json
+#pileup=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/PileUp/pileup_latest.txt
+pileup=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/PileUp/pileup_latest.txt 
+#pileup=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/PileUp/pileup_latest.txt 
+bins=99
 minBias="69200"
 uncert="1.046"
 
